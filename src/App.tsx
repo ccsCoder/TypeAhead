0965cb44f5e77c55675ea256e382d82f42ef8cb7
Nishant Gupta <ccsCoder@gmail.com>
2023-02-11 22:57:28 +0000

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import './App.css'
+
+export default function App() {
+  return (
+    <main>
+      <h1>Hello Repl.it world</h1>
+    </main>
+  )
+}
---
+++ src/service.ts
@@ -0,0 +1,16 @@
+
+
+const Uri = new URL('https://api.first.org/data/v1/countries');
+
+export declare interface Country {
+  name: string,
+  code: string,
+}
+
+const search = (predicate: string): Country[] => {
+  uri.
+  fetch()
+}
+
+export default search;
+
